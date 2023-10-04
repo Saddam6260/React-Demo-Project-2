220ad21a0901e2e03fa83884d7c0e21aa89dea32
Saddam Hafiz Alif <mr.alif.dev@gmail.com>
2023-10-04 15:02:29 +0600

    added no items condition

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,20 @@ function App() {
     })
   };
 
+  let content = (
+    <p className="no-user">No Such File Here</p>
+  )
+
+  if(usersInfo.length > 0) {
+    content = (
+      <UserList usersList={usersInfo} />
+    )
+  }
+
   return (
     <div className="container">
       <UserInput addUser={addUserHandler} />
-      <UserList usersList={usersInfo} />
+      {content}
     </div>
   );
 }
